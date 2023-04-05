bc3f250e004016ad6f8ba846b9f4eadfe8c94fcf
fahmianjani <rimurute7@gmail.com>
2023-04-05 18:02:22 +0700

    pertama commit

---
+++ database/migrations/2023_04_03_113428_create_accounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('users');
+            $table->text('address_1');
+            $table->text('address_2')->nullable();
+            $table->integer('telp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
